28ead3d9a842188eb1608cca9d9489c208d6ca2c
hhhtm5 <ghnxn2tzqh@privaterelay.appleid.com>
2026-04-09 21:13:25 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,19 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { useEffect, useState } from 'react';
 import Home from './pages/Home';
-import Services from './pages/Services';
-import Booking from './pages/Booking';
-import { useTelegram } from './hooks/useTelegram';
-import { authTelegram } from './api';
+import Rent from './pages/Rent';
+import Packages from './pages/Packages';
+import Mixing from './pages/Mixing';
+import Question from './pages/Question';
 
 function App() {
-  const { tg, user } = useTelegram();
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-  useEffect(() => {
-    if (tg && user) {
-      const initData = tg.initData;
-      authTelegram(initData)
-        .then(res => {
-          localStorage.setItem('token', res.data.token);
-          setIsAuthenticated(true);
-        })
-        .catch(err => console.error('Auth error', err));
-    }
-  }, [tg, user]);
-
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/booking" element={<Booking />} />
+        <Route path="/rent" element={<Rent />} />
+        <Route path="/packages" element={<Packages />} />
+        <Route path="/mixing" element={<Mixing />} />
+        <Route path="/question" element={<Question />} />
       </Routes>
     </BrowserRouter>
   );
